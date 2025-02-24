62c776b276af3be6c47ef97aded8bd3ce31b6844
Devraj-hub69 <devrajmaity2004@gmail.com>
2025-02-24 13:26:29 +0530

    paytm

---
+++ Day9/classwork/index.js
@@ -0,0 +1,36 @@
+// {
+//     let a=10;
+//     console.log(a);
+// }
+// let a = 10,b=20;
+// conseole.log(a+=a)
+// if (a==b){
+//     console.log("a is equal to b")
+// }
+// else{
+//     console.log("a is not equal to b")
+// }
+// function myfunction(a,b){
+//     console.log(a+b)
+// }
+// let a1=10;
+// // let b1=20;
+// myfunction(a,b)
+// const myfunc=(a,b)=>{
+//     console.log(a+b)
+// }
+// myfunc(a,b)
+// console.log(name)
+// var name="alexander"
+let $name="jjhh"
+ //let d="   "
+console.log(d.length)
+let n1="devraj",n2="maity"
+console.log(n1,n2)
+let d="    ";
+console.log(d.length)
+let fName="Sudip";
+let lName="Pradhan";
+console.log(fName,lName);
+let b=20,c="30";
+console.log(b+c);
